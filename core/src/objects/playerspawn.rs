45bbe897d7fe4e85b814856aa4f6a975e67866a3
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2022-10-16 14:46:19 -0400

    idk

--- core/src/objects/playerspawn.rs
+++ core/src/objects/playerspawn.rs
@@ -1,10 +1,12 @@
 use std::{cell::RefCell, rc::Rc};
 
 use crate::{structures::*, Celeste};
+
+use super::player::Player;
 pub struct PlayerSpawn {
     target: f32,
     state: u8,
-    delay: u8,
+    delay: i8,
     djump: u8,
 }
 impl PlayerSpawn {
@@ -27,6 +29,7 @@ impl PlayerSpawn {
                 delay: 0,
                 state: 0,
                 djump: celeste.max_djump,
+                target: y,
             }))),
             draw: Self::draw,
             update: Self::update,
@@ -40,7 +43,7 @@ impl PlayerSpawn {
             _ => unreachable!(),
         };
         let mut this = tref.borrow_mut();
-        if this.state == 0 && obj.pos.y < this.target + 16 {
+        if this.state == 0 && obj.pos.y < this.target + 16.0 {
             this.state = 1;
             this.delay = 3;
         } else if this.state == 1 {
@@ -52,6 +55,21 @@ impl PlayerSpawn {
                 obj.pos.y = this.target;
                 obj.spd = Vector { x: 0.0, y: 0.0 };
                 this.state = 2;
+                this.delay = 5;
+                celeste.shake = 5;
+                // init smoke
+                // sfx 5
+            }
+        } else if this.state == 2 {
+            this.delay -= 1;
+            obj.spr = 6;
+            if this.delay < 0 {
+                celeste.objects.push(Rc::new(RefCell::new(Player::init(
+                    celeste,
+                    obj.pos.x,
+                    this.target,
+                ))));
+                obj.destroy_self(celeste);
             }
         }
     }
